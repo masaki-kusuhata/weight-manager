c940ab8262fe8a9d78d74ab97a9984c21e786ee4
FKN_kusuhata <kusuhata.masaki@jp.fujitsu.com>
2016-10-12 14:49:50 +0900

    first commit

--- src/main.aot.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './modules/app.module.ngfactory';
-const platform = platformBrowser();
-platform.bootstrapModuleFactory(AppModuleNgFactory);
